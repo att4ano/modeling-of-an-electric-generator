d049041fcb9047c809de5b30718ccc82733830a5
att4ano <113085241+att4ano@users.noreply.github.com>
2024-03-12 17:33:48 +0300

    add src

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GeneratorSimulation from './GeneratorSimulation';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Моделирование электрогенератора</h1>
+            <GeneratorSimulation />
+        </div>
+    );
+};
+
+export default App;
